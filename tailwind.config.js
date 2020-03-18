5d949be60718d93926e701ca05cdb8efac08ea2c
Roelof Jan Elsinga <roelofjanelsinga@gmail.com>
2020-03-18 11:40:18 +0100

    Updated build steps

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                blue: '#0000fa',
+                'light-gray': '#f5f6fd'
+            }
+        }
+    }
+};
